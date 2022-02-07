a09ed48fe06c02e55b164117d28a3aec2c7baa98
Pascal CHRISTOPHE <pascal.christophe51@gmail.com>
2022-02-07 15:13:56 +0100

    text

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -18,7 +18,7 @@ racine = tk.Tk()
 racine.title("Sandpiles")
 racine.geometry("1080x720")
 
-bouton = tk.Button(txt="aléatoire")
+bouton = tk.Button(text="aléatoire")
 bouton.grid(row=1, column=1)
 #bjr
 racine.mainloop()
