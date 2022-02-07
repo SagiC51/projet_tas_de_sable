0c4422e9578c9f3c558847431f8bdc2bc174332f
Pascal CHRISTOPHE <pascal.christophe51@gmail.com>
2022-02-07 15:35:39 +0100

    Supr

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -18,7 +18,6 @@ def aleat():
 racine = tk.Tk()
 racine.title("Sandpiles")
 racine.geometry("720x720")
-
 bouton = tk.Button(text="aléatoire")
 bouton.grid(row=1, column=1, columnspan= 1, rowspan=1)
 racine.mainloop()
