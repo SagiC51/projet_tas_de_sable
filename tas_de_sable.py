2e17cda4943a9544530feafb175792594d4778a7
uvsq22018937 <mohammed.chakroun@ens.uvsq.fr>
2022-02-07 15:08:21 +0100

    1

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -5,3 +5,20 @@
 # KALI-A-NGOUELE Gloire
 # https://github.com/uvsq21918050/projet_tas_de_sable
 #########################################
+
+###------Import des modules------###
+import tkinter as tk
+
+###------Variables globales------###
+###------Fonctions------###
+
+###------Programme principale------###
+
+racine = tk.Tk()
+racine.title("Sandpiles")
+racine.geometry("1080x720")
+
+bouton = tk.Button(txt="aléatoire")
+bouton.grid(row=1, column=1)
+
+racine.mainloop()
