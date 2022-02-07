83d101ed54014b5f1a3230351fef54d9d6adf5ba
Pascal CHRISTOPHE <pascal.christophe51@gmail.com>
2022-02-07 15:27:46 +0100

    maj

---
+++ tas_de_sable.py
@@ -0,0 +1,26 @@
+#########################################
+# groupe MPCI TD1
+# CHRISTOPHE Passcal
+# CHAKROUN Mohammed
+# KALI-A-NGOUELE Gloire
+# https://github.com/uvsq21918050/projet_tas_de_sable
+#########################################
+
+#------Import des modules------###
+import tkinter as tk
+
+#------Variables globales------###
+#------Fonctions------###
+def aleat():
+    pass
+#------Programme principale------###
+
+racine = tk.Tk()
+racine.title("Sandpiles")
+racine.geometry("1080x720")
+
+bouton = tk.Button(text="aléatoire")
+bouton.grid(row=1, column=1)
+#bjr
+
+racine.mainloop()
