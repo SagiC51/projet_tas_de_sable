8c481a3ba06484c9d2d6e6bf87fd08f22ba25123
Pascal CHRISTOPHE <pascal.christophe51@gmail.com>
2022-02-07 15:46:04 +0100

    Canvas

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -18,6 +18,8 @@ def aleat():
 racine = tk.Tk()
 racine.title("Sandpiles")
 racine.geometry("720x720")
+Canvas = tk.Canvas(bg= 'white', height= 720, width= 720)
 bouton = tk.Button(text="aléatoire")
+Canvas.grid(row=1, column=1, columnspan= 1, rowspan=1)
 bouton.grid(row=1, column=1, columnspan= 1, rowspan=1)
 racine.mainloop()
